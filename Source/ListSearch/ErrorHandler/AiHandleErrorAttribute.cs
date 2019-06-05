d39c385d27c0a390a4a1ebe7415c775f4c84df3d
Adrian Solis <freddiemcdog@hotmail.com>
2019-06-04 21:57:01 -0700

    Fix line endings

--- Source/ListSearch/ErrorHandler/AiHandleErrorAttribute.cs
+++ Source/ListSearch/ErrorHandler/AiHandleErrorAttribute.cs
@@ -2,12 +2,32 @@
 // Copyright (c) Microsoft. All rights reserved.
 // </copyright>
 
-namespace ListSearch.ErrorHandler{    using System;    using System.Web.Mvc;    using Microsoft.ApplicationInsights;
+namespace ListSearch.ErrorHandler
+{
+    using System;
+    using System.Web.Mvc;
+    using Microsoft.ApplicationInsights;
 
     /// <summary>
     /// Application Insights error filter
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
     public class AiHandleErrorAttribute : HandleErrorAttribute
-    {        /// <inheritdoc/>
-        public override void OnException(ExceptionContext filterContext)        {            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)            {                // If customError is Off, then AI HTTPModule will report the exception                if (filterContext.HttpContext.IsCustomErrorEnabled)                {                    var ai = new TelemetryClient();                    ai.TrackException(filterContext.Exception);                }            }            base.OnException(filterContext);        }    }}
+    {
+        /// <inheritdoc/>
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)
+            {
+                // If customError is Off, then AI HTTPModule will report the exception
+                if (filterContext.HttpContext.IsCustomErrorEnabled)
+                {
+                    var ai = new TelemetryClient();
+                    ai.TrackException(filterContext.Exception);
+                }
+            }
+
+            base.OnException(filterContext);
+        }
+    }
+}
